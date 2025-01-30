a61ea8c555da545be83f21aa86778da4d0218101
JT <jstorbit@gmail.com>
2025-01-29 20:11:58 -0700

    added second function to store variable from prompt and display it after

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,3 +3,9 @@ function multiply (firstnum, secondnum) {
    alert("2 times 4 is " + product);
    return
 }
+
+function saymyname () {
+   var myname = prompt("Please enter your name:", myname);
+   alert("Hi " + myname);
+   return
+}
